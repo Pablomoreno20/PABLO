54db590d53d624c1b4478aea0c2d9c3da23a0687
Pablomoreno20 <142330504+Pablomoreno20@users.noreply.github.com>
2023-08-15 10:21:44 -0600

    Add files via upload

---
+++ Generacion de compu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (int argc,char **argv)
+{
+	printf("Primera generación 1940 hasta 1956 Estas computadoras utilizaban válvulas de vacío para procesar información\n");
+    printf("Segunda generación, desde 1956 hasta 1963: Computadoras de estado sólido con transistores.\n");
+    printf("Tercera Generación, de 1964 hasta 1971: Computadoras de estado sólido con circuitos Integrados.\n");
+	return 0;
+}
