c6515bff744ad1eeb8e8af552041eccb988be132
Pablomoreno20 <142330504+Pablomoreno20@users.noreply.github.com>
2023-11-23 10:55:51 -0600

    Add files via upload

---
+++ parcial 2 reposicion.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int fibonacci(int n)
+{
+  if (n <= 1)
+  {
+    return n;
+  }
+
+  int anterior = 0;
+  int actual = 1;
+
+  for (int i = 2; i <= n; i++)
+  {
+    int newactual = anterior + actual;
+    anterior = actual;
+    actual = newactual;
+  }
+  if ( newactual > n)
+ {
+
+  return actual;
+ }
+}
+
+int main()
+{
+  int n;
+
+  srand(time(NULL));
+
+  n = 100000;
+
+  printf("Calculando las secuencias fibonacci\n");
+
+  for (int i = 0; i < n; i++)
+  {
+    int secuencia = i + 1;
+    int numero = fibonacci(i);
+
+    printf("Secuencia %d: %d\n", secuencia, numero);
+  }
+
+  return 0;
+}
